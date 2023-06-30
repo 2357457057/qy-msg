28a734ab53a7df71274658c7e09c2c027dead145
SeverusQy <yangyongji123@live.con>
2023-07-01 04:22:43 +0800

    MsgServer

--- src/main/java/top/yqingyu/qymsg/netty/MsgServer.java
+++ src/main/java/top/yqingyu/qymsg/netty/MsgServer.java
@@ -13,9 +13,12 @@ public class MsgServer {
     private int pool = Runtime.getRuntime().availableProcessors() * 2;
     private int radix = 32;
     private long clearTime = 30 * 60 * 1000;
+    private int bodyLengthMax = 1400;
     private String serverName = "QyMsg";
     private String threadName = "handle";
     private Class<? extends QyMsgServerHandler> handler;
+    private ServerExceptionHandler exceptionHandler;
+    private MsgTransfer msgTransfer;
     private ChannelFuture future;
 
     private MsgServer() {
@@ -29,27 +32,27 @@ public class MsgServer {
         }
 
 
-        public Builder Pool(int pool) {
+        public Builder pool(int pool) {
             msgServer.pool = pool;
             return this;
         }
 
-        public Builder Radix(int radix) {
+        public Builder radix(int radix) {
             msgServer.radix = radix;
             return this;
         }
 
-        public Builder ClearTime(int clearTime) {
+        public Builder clearTime(int clearTime) {
             msgServer.clearTime = clearTime;
             return this;
         }
 
-        public Builder ServerName(String serverName) {
+        public Builder serverName(String serverName) {
             msgServer.serverName = serverName;
             return this;
         }
 
-        public Builder ThreadName(String threadName) {
+        public Builder threadName(String threadName) {
             msgServer.threadName = threadName;
             return this;
         }
@@ -59,13 +62,32 @@ public class MsgServer {
             return this;
         }
 
+        public Builder exceptionHandler(ServerExceptionHandler exceptionHandler) {
+            msgServer.exceptionHandler = exceptionHandler;
+            return this;
+        }
+
+        public Builder transfer(MsgTransfer msgTransfer) {
+            msgServer.msgTransfer = msgTransfer;
+            return this;
+        }
+
+        public Builder bodyLengthMax(int bodyLengthMax) {
+            msgServer.bodyLengthMax = bodyLengthMax;
+            return this;
+        }
+
         public MsgServer build() throws Exception {
             NioEventLoopGroup serverGroup = new NioEventLoopGroup(1, ThreadUtil.createThFactoryC("BOSS", "Th"));
             NioEventLoopGroup clientGroup = new NioEventLoopGroup(msgServer.pool, ThreadUtil.createThFactoryC(msgServer.serverName, msgServer.threadName));
             ServerBootstrap serverBootstrap = new ServerBootstrap();
             serverBootstrap.group(serverGroup, clientGroup);
             serverBootstrap.channel(NioServerSocketChannel.class);
-            serverBootstrap.childHandler(new QyMsgServerInitializer(msgServer.handler, MsgTransfer.init(msgServer.radix, msgServer.clearTime)));
+            msgServer.msgTransfer = MsgTransfer.init(msgServer.radix, msgServer.bodyLengthMax, msgServer.clearTime);
+            QyMsgServerInitializer initializer = new QyMsgServerInitializer(msgServer.handler, msgServer.msgTransfer);
+            initializer.setQyMsgExceptionHandler(msgServer.exceptionHandler == null ? new ServerExceptionHandler() {
+            } : msgServer.exceptionHandler);
+            serverBootstrap.childHandler(initializer);
             msgServer.serverBootstrap = serverBootstrap;
             return msgServer;
         }
@@ -79,4 +101,47 @@ public class MsgServer {
         future.channel().closeFuture().sync();
     }
 
+    public ServerBootstrap getServerBootstrap() {
+        return serverBootstrap;
+    }
+
+    public int getPool() {
+        return pool;
+    }
+
+    public int getRadix() {
+        return radix;
+    }
+
+    public long getClearTime() {
+        return clearTime;
+    }
+
+    public String getServerName() {
+        return serverName;
+    }
+
+    public String getThreadName() {
+        return threadName;
+    }
+
+    public Class<? extends QyMsgServerHandler> getHandler() {
+        return handler;
+    }
+
+    public ServerExceptionHandler getExceptionHandler() {
+        return exceptionHandler;
+    }
+
+    public MsgTransfer getMsgTransfer() {
+        return msgTransfer;
+    }
+
+    public ChannelFuture getFuture() {
+        return future;
+    }
+
+    public int getBodyLengthMax() {
+        return bodyLengthMax;
+    }
 }
